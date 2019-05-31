7da2cb2da484212800046f3f7f00da1693da0e85
Frederic G. MARAND <fgm@osinet.fr>
2019-05-31 18:02:49 +0200

    WIP whoami test.

--- sv/serverTests.js
+++ sv/serverTests.js
@@ -1,6 +1,16 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 
+import { Accounts } from 'meteor/accounts-base';
+import { Match } from 'meteor/check';
+import { Log } from 'meteor/logging';
+import { Meteor } from 'meteor/meteor';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+import { WebApp } from 'meteor/webapp';
+
+import { DrupalBase } from "../shared/DrupalBase";
 import { DrupalConfiguration } from './DrupalConfiguration';
+import { DrupalServer } from "./DrupalServer";
 
 const SERVICE_NAME = 'mock-service';
 
@@ -18,6 +28,33 @@ function mockSettings(serviceName) {
   mockConfiguration[serviceName] = {};
   mockConfiguration.public[serviceName] = {};
   return mockConfiguration;
+}
+
+function mockServer(getContent, fail = false) {
+  let mockHttp = {
+    get: sinon.fake.returns({
+      "content": JSON.stringify(getContent),
+    }),
+  };
+  const meteor = {
+    Collection: sinon.fake(),
+    settings: {
+      [SERVICE_NAME]: {},
+      public: {
+        [SERVICE_NAME]: {},
+      },
+    },
+    users: null,
+  };
+  const server =  new DrupalServer(null, meteor, Log, null, null, null, null, mockHttp, JSON);
+  expect(server).not.to.be.null;
+  expect(server).to.be.an.instanceOf(DrupalServer);
+  if (fail) {
+    server.http.get = sinon.fake.returns({
+      "content": {},
+    })
+  }
+  return server;
 }
 
 const testCorrectConfiguration = function () {
@@ -72,7 +109,32 @@ const testIncorrectConfiguration = function () {
   expect(instantiation).to.throw(ServiceConfiguration.ConfigError, `Service ${SERVICE_NAME} not configured`);
 };
 
+const testWhoamiHappy = function(done) {
+  const uid = 1;
+  const server = mockServer({
+    online: true,
+    uid,
+    name: "admin",
+    roles: [],
+  });
+  const whoami = server.whoamiMethod('', '');
+  expect(whoami).not.to.be.null;
+  expect(whoami).to.have.nested.property('uid', uid);
+  done();
+};
+
+const testWhoamiSad = function(done) {
+  const uid = 1;
+  const server = mockServer({ online: true }, true);
+  const whoami = server.whoamiMethod('', '');
+  expect(whoami).not.to.be.null;
+  expect(whoami).to.have.nested.property('uid', uid);
+  done();
+};
+
 export {
   testCorrectConfiguration,
   testIncorrectConfiguration,
+  testWhoamiHappy,
+  testWhoamiSad,
 };
--- sv/tests.js
+++ sv/tests.js
@@ -1,4 +1,9 @@
-import { testCorrectConfiguration, testIncorrectConfiguration } from './serverTests';
+import {
+  testCorrectConfiguration,
+  testIncorrectConfiguration,
+  testWhoamiHappy,
+  testWhoamiSad,
+} from './serverTests';
 
 if (!Meteor.isPackageTest) {
   throw new Error('This code should only run in package test mode.');
@@ -8,4 +13,9 @@ if (!Meteor.isPackageTest) {
 describe('Legacy configuration-related tests ported from Tinytest', function () {
   it('should accept correct configuration', testCorrectConfiguration);
   it('should reject incorrect configuration', testIncorrectConfiguration);
+});
+
+describe('Server tests', function () {
+  it('should return expected whoami values in happy cases', testWhoamiHappy);
+  it('should return expected whoami values in sad cases', testWhoamiSad);
 });
